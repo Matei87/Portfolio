b7d6fbc1f9000baf5772c3fb028548c4e8440b53
Matei Mircea <mrc_matei@yahoo.com>
2020-07-16 16:40:27 +0300

    Updates

---
+++ precache-manifest.a54051c90edf24a0112924de960a689d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5722f0699ddb47aee3d3359aaf0c0b59",
+    "url": "/Portofolio/index.html"
+  },
+  {
+    "revision": "9795a1c9020b36183ae1",
+    "url": "/Portofolio/static/css/2.37620a9f.chunk.css"
+  },
+  {
+    "revision": "e7a96dea812200970698",
+    "url": "/Portofolio/static/css/main.d7b39cf2.chunk.css"
+  },
+  {
+    "revision": "9795a1c9020b36183ae1",
+    "url": "/Portofolio/static/js/2.538a0c00.chunk.js"
+  },
+  {
+    "revision": "31ac725c350433707f281916a808f78f",
+    "url": "/Portofolio/static/js/2.538a0c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a96dea812200970698",
+    "url": "/Portofolio/static/js/main.15e5c2a8.chunk.js"
+  },
+  {
+    "revision": "ea7818537f0bd195c62b",
+    "url": "/Portofolio/static/js/runtime-main.18786445.js"
+  },
+  {
+    "revision": "d7722e47c6cba869efd322e5c5fd1a9c",
+    "url": "/Portofolio/static/media/blog.d7722e47.png"
+  },
+  {
+    "revision": "cc0f70f4a44af6384d23239520559f5b",
+    "url": "/Portofolio/static/media/cinema.cc0f70f4.png"
+  },
+  {
+    "revision": "a338982fd944ee39709d7853a6dd23b6",
+    "url": "/Portofolio/static/media/coding.a338982f.png"
+  },
+  {
+    "revision": "378f2d6b2c0507eb3947ba5b6dafcde9",
+    "url": "/Portofolio/static/media/games.378f2d6b.png"
+  },
+  {
+    "revision": "80acb5ccc8f418fbbfcc03e0313d93fa",
+    "url": "/Portofolio/static/media/gym.80acb5cc.jpeg"
+  },
+  {
+    "revision": "b18e5af3dfc5dd224b901dbb3c0c445e",
+    "url": "/Portofolio/static/media/header-background.b18e5af3.jpg"
+  },
+  {
+    "revision": "05a6000b81678222afe415887813cef5",
+    "url": "/Portofolio/static/media/me.05a6000b.png"
+  },
+  {
+    "revision": "5425fa5f033a6e6126bfb0865228f777",
+    "url": "/Portofolio/static/media/myresume.5425fa5f.pdf"
+  },
+  {
+    "revision": "0245cfdd47d00b0f46cefef5615f421b",
+    "url": "/Portofolio/static/media/site1.0245cfdd.png"
+  },
+  {
+    "revision": "c6a18de43145978d5465003940ebd60e",
+    "url": "/Portofolio/static/media/weather.c6a18de4.png"
+  }
+]);
